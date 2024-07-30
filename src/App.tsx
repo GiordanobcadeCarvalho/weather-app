7df81a6ed5051da3272210f7a4df03ba51f9d25d
GiordanobcadeCarvalho <57145039+GiordanobcadeCarvalho@users.noreply.github.com>
2024-07-30 19:33:30 -0300

    fix: remove-react-fc

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import React from "react";
 import { WeatherDashboard } from "./components/WeatherDashboard";
 
-const App: React.FC = () => {
+const App = () => {
   return (
     <div>
       <WeatherDashboard />
